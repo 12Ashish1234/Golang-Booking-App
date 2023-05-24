a5806c885769fe10b679a3f9aeef0919500a2af1
Ashish <ashishd52280@gmail.com>
2023-05-24 20:39:56 +0530

    Tutorial with arrays & slices, loops and if condition

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
 	var conferenceName = "Go conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
+	var bookings []string
 
 	/*
 		We can declare variables using := syntax as shown below
@@ -17,7 +21,6 @@ func main() {
 
 	/*
 			To display the datatype of a variable, use %T placeholder
-
 
 		fmt.Printf("conferenceRickets is %T \n", conferenceTickets)
 		fmt.Printf("conferenceName is %T \n", conferenceName)
@@ -32,26 +35,61 @@ func main() {
 	*/
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Printf("Get your tickets here \n \n")
+	fmt.Printf("Get your tickets here to attend\n \n")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	/*
+		Loops
+	*/
 
-	// User Inputs
+	for {
+		/*
+			Arrays and Slices
+			Slices are defined similar to arrays but only change is size definition is not given.
+		*/
+		// var bookings [50]string
 
-	fmt.Println("Enter your First Name:")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter your last Name:")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter your Email:")
-	fmt.Scan(&email)
-	fmt.Println("Enter no of tickets")
-	fmt.Scan(&userTickets)
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	remainingTickets = remainingTickets - userTickets
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive the tickets in your email address %v\n \n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets are remaining\n", remainingTickets)
+		// User Inputs
 
+		fmt.Println("Enter your First Name:")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your last Name:")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter your Email:")
+		fmt.Scan(&email)
+		fmt.Println("Enter no of tickets")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("The whole slice: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("The Array type: %T\n", bookings)
+		fmt.Printf("Array Length: %v\n", len(bookings))
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive the tickets in your email address %v\n \n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets are remaining\n", remainingTickets)
+
+		/*
+			Below logic is for printing only the first names instead of the whole name
+		*/
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("The first names of our bookings: %v \n \n", firstNames)
+
+		// noTicketsRemain := remainingTickets == 0
+		if remainingTickets == 0 {
+			fmt.Println("Our conference is booked out")
+			break
+		}
+	}
 }
